9ff6827f8a5e44546db515a3897b50197965ddb5
wdd <630137139@qq.com>
2026-02-05 15:15:20 +0800

    perf: ESLint Prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,15 +1,18 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import unocss from '@unocss/eslint-config/flat'
 
 export default [
   { ignores: ['dist', 'node_modules', 'coverage', '*.config.js'] },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.{ts,tsx}'],
+    ...reactPlugin.configs.flat.recommended,
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -20,10 +23,7 @@ export default [
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
@@ -35,4 +35,8 @@ export default [
       'quote-props': ['warn', 'as-needed'],
     },
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  unocss,
+  eslintConfigPrettier,
 ]
